350f37f6a4f8c1eff12f05b3a05f25c206623036
Marcio Alberto De Oliveira Lourenco <marcio.lourenco@proway.treina>
2022-12-12 22:04:20 -0300

    add login e logout

--- Python aulas/aula 12-07/main.py
+++ Python aulas/aula 12-07/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from pessoa import Pessoa
 
 pessoa1 = Pessoa('Marcio', '28', '1,82')
@@ -7,13 +7,17 @@ pessoa3 = Pessoa('Adriano', '30', '1,80')
 
 lista = [pessoa1, pessoa2, pessoa3]
 app = Flask(__name__)
+app.secret_key = 'today'
 
-@app.route('/')
+@app.route('/listar')
 def inicio():
-    return render_template('index.html', titulo = 'Lista Pessoas', pessoas = lista)
+    return render_template('lista.html', titulo = 'Lista de Pessoas', pessoas = lista)
 
-@app.route('/novo')
+@app.route('/casdastrar')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+
     return render_template('novo.html', titulo = 'Cadastro Pessoa')
 
 @app.route('/criar', methods = ['POST'])
@@ -28,5 +32,26 @@ def criar():
 
     return redirect('/')
 
+@app.route('/')
+def login():
+    return render_template('login.html', titulo = "Login")
+
+@app.route('/autenticar', methods = ['POST'])
+def autenticar():
+    if 'today' == request.form['senha']:
+
+        session['usuario_logado'] = request.form['usuario']
+
+        flash(session['usuario_logado'] + ' Você está logado')
+        return redirect('/listar')
+    else:
+        flash('Senha Invalida')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] == None
+    flash('VOCE FOi DESCONECTADO')
+    return redirect('/')
 
 app.run(debug=True)
